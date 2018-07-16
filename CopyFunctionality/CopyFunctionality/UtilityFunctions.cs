401746b5e9e91084d35ffdcf8a7c24e17820f661
Peter N K <pnk@avb.bg>
2018-07-16 13:51:53 +0300

    This test was created in version 2.0.40

---
+++ CopyFunctionality/CopyFunctionality/UtilityFunctions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CopyFunctionality
+{
+
+    public class ReportAction
+    {
+        public static void PositiveReport()
+        {                    
+                        
+            Ranorex.Report.Success("Sucessfully Performed");
+        }
+
+        public static void NegativeReport(double dActualValue, double dHardcodedVAlue, int iErrorLine)
+        {
+            string strErrLine = "Error on Line: " + (iErrorLine + 1);
+            Ranorex.Report.Error(strErrLine);
+
+            string strActualValue = "Actual Value is:" + dActualValue;
+            Ranorex.Report.Error(strActualValue);
+
+            string strhardcodedValue = "HardcodedValue is:" + dHardcodedVAlue;
+            Ranorex.Report.Error(strhardcodedValue);
+        }
+    }
+    public class Mathematics
+    {
+     
+        public static double GetNunbertsAbsDifference(double first, double second)
+        {
+            double dDiference = first - second;
+
+            return System.Math.Abs(dDiference);
+        }
+
+       
+      
+        
+    }
+}
+
+
+
